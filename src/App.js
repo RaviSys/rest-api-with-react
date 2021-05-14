ae79f61a9459df464a779f41283c2631482b5e2a
Ravi Singh <ravi@appsimpact.com>
2021-05-14 18:01:12 +0530

    working with useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect, useCallback } from 'react';
 import MoviesList from './MoviesList';
 import Layout from './Layout';
 
@@ -8,20 +8,29 @@ function App() {
 
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
+  // fetch('https://swapi.dev/api/films/').then(response => {
+  //   return response.json();
+  // }).then(data => {
+  //   setMovies(data.results);
+  // });
+
+  const fetchMoviesHandler = useCallback(() => {
     fetch('https://swapi.dev/api/films/').then(response => {
       return response.json();
     }).then(data => {
       setMovies(data.results);
     });
-  };
+  });
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <Layout>
       <header className="App-header">
         <h1 className="header-title">Let's get sratrted with RESTful APIs Integration</h1>
       </header>
-      <button type="button" onClick={fetchMoviesHandler}>Load Movies</button>
       <MoviesList movies={movies} />
     </Layout>
   );
